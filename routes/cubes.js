9e7e4fcba8a3a18d7bb5fd9fee2ce3d239d77c14
ProMD <martin.dietrich@pro-md>
2016-01-03 17:38:28 +0000

    final version on vacation

--- routes/cubes.js
+++ routes/cubes.js
@@ -17,11 +17,31 @@ router.get('/', function(req, res) {
 });
 
 /*
- * Receive uüdates from cubes
+ * Receive updates from cubes
  */
-router.post('/:id', function(req, res) {
-    console.log(req.body);    
-    res.json({"status" : "ok"});
+router.post('/:id', function(req, res) {    
+    db = req.db;
+    // { "temp": "28", "voltage": "3945", "humidity": "60", "event" : "scheduled" }
+    var content = req.body;
+    content.cube = req.params.id;
+    content.time = Date.now();
+    console.log(req.body);
+
+    if (content.event == 'motion') {
+         // movement, room is blocked
+         db.select().from('rooms').containsText({cubes: content.cube}).all()
+         .then(function (rooms) {
+             console.log('busy room(s)', rooms);
+	     for (var i in rooms) {
+                // change the status to busy
+	     }
+         });
+    }
+
+    db.insert().into('history').set(content).one()
+    .then(function (user) {
+        res.json({"status" : "ok"});
+    });
 });
 
 module.exports = router;
