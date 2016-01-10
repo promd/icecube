0d932f656661e8364faff348e0de955fabd9ca79
Martin D <martin.dietrich@pro-md.de>
2016-01-10 17:20:42 +0100

    added MAG and switched LED port

--- luascripts/init.lua
+++ luascripts/init.lua
@@ -1,11 +1,13 @@
 -- SOS pin. pull up to delete init.lua
 sos = 4 --GPIO 2
 -- LED pin
-led = 7 --GPIO 13
+led = 1 --GPIO 5
 -- DHT11 pin
 dhtpin = 6 --GPIO 12
 -- PIR pin
 pir = 5 --GPIO 14
+-- Magnetic sensor
+mag = 7 --GPIO 13
 
 -- Wifi and server Details
 ssid   = "ic"
@@ -16,7 +18,7 @@ cube   = "proto1"
 server = "192.168.90.1"
 port   = 3000
 -- version of this script
-ver    = "0.0.2"
+ver    = "0.0.3"
 
 ------ NO CONFIGURATION BELOW ----------
 
@@ -25,8 +27,7 @@ ws2812.writergb(led, string.char(0, 0, 0))
 
 -- Function to gather information and send a message
 function sendMessage(event)
-    print("[PROG] SendMessage() started")
-    ws2812.writergb(led, string.char(0, 100, 0))
+    print("[PROG] SendMessage("..event..") started")
     -- get voltage in mV (internal ADC pin!)
     volt = adc.readvdd33()
     
@@ -69,12 +70,12 @@ function sendMessage(event)
     conn:on("disconnection", function(conn, pl) 
         print("[WiFi] Putting WiFi at sleep")
         conn = nil
+        ws2812.writergb(led, string.char(0, 0, 100))
         wifi.sleeptype(wifi.MODEM_SLEEP)
     end)
     conn:on("receive", function(conn, pl) 
         print("[PORT] Receiving Data")
         print(pl) 
-        ws2812.writergb(led, string.char(100, 0, 0))
         conn:close()
     end)
     conn:on("sent", function(conn, pl) 
@@ -90,27 +91,28 @@ function sendMessage(event)
     
 end
 
--- pulsing LED (not working at current GPIO ??
-r = 0
-dir = 1
---tmr.alarm(0, 10, 1, function() 
---    print(r.." : "..dir)
---    if dir == 1  then r = r+1
---    else r = r-1 end
---    if r == 100 then dir = 0
---    elseif r == 0 then dir = 1
---    end 
---    ws2812.writergb(led, string.char(0, r, 0))
---end)
-
 --register WiFi callbacks
 wifi.sta.eventMonReg(wifi.STA_IDLE, function() print("[WiFi] STATION_IDLE") end)
-wifi.sta.eventMonReg(wifi.STA_CONNECTING, function() print("[WiFi] STATION_CONNECTING") end)
+wifi.sta.eventMonReg(wifi.STA_CONNECTING, function() 
+    print("[WiFi] STATION_CONNECTING") 
+    r = 0
+    dir = 1
+    tmr.alarm(0, 10, 1, function() 
+        if dir == 1  then r = r+1
+        else r = r-1 end
+        if r == 100 then dir = 0
+        elseif r == 0 then dir = 1
+        end 
+        ws2812.writergb(led, string.char(r, 0, 0))
+    end)    
+end)
 wifi.sta.eventMonReg(wifi.STA_WRONGPWD, function() print("[WiFi] STATION_WRONG_PASSWORD") end)
 wifi.sta.eventMonReg(wifi.STA_APNOTFOUND, function() print("[WiFi] STATION_NO_AP_FOUND") end)
 wifi.sta.eventMonReg(wifi.STA_FAIL, function() print("[WiFi] STATION_CONNECT_FAIL") end)
 wifi.sta.eventMonReg(wifi.STA_GOTIP, function() 
     print("[WiFi] STATION_GOT_IP : "..wifi.sta.getip()) 
+    tmr.stop(0)
+    ws2812.writergb(led, string.char(0, 100, 0))
     sendMessage("startup")
 end)
 
@@ -145,4 +147,14 @@ gpio.mode(pir,gpio.INT)
 gpio.trig(pir,"up",function()
     print("--- MOTION DETECTED ---")
     sendMessage("motion")
+end)
+
+-- init MAG (our 2nd Trigger - keep at the end)
+print("[MAG] starting event monitoring") 
+gpio.mode(mag, gpio.OUTPUT)
+gpio.write(mag, gpio.LOW)
+gpio.mode(mag,gpio.INT)
+gpio.trig(mag,"up",function()
+    print("--- DOOR OPEN ---")
+    sendMessage("door")
 end)
